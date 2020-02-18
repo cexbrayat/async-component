3f4e1d2d965eb62565ef8db71ae2e866083f8c51
cexbrayat <cedric@ninja-squad.com>
2020-02-18 23:31:13 +0100

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Hello from './components/Hello.vue';
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Hello
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Hello</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Hello"
+});
+</script>
